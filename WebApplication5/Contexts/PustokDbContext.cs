178738e6ca12036bd4b480a8402dead82f9b0b72
leman <mi7NJBDX9@code.edu.az>
2023-12-05 13:45:20 +0400

    createPustok

---
+++ WebApplication5/Contexts/PustokDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+
+namespace Pustok.Contexts;
+
+public class PustokDbContext:DbContext
+{
+    public DbSet<Slider> sliders { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Servere=DESKTOP-QJ2FRIC\\SQLEXPRESS;Database=Pustok;Trusted_Connection=true");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ WebApplication5/Program.cs
@@ -0,0 +1,36 @@
+namespace WebApplication5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+         
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapDefaultControllerRoute();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
